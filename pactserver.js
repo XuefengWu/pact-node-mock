6ed28edfe62dee6c344e2cdd1329ad04ee2523ab
Xuefeng.Wu <benewu@gmail.com>
2014-02-10 11:55:20 +0800

    fix if can not find response

--- pactserver.js
+++ pactserver.js
@@ -1,4 +1,7 @@
 var http = require('http');
+
+
+  
 var interactions;
 
 var fs = require('fs');
@@ -14,25 +17,29 @@ fs.readFile(file, 'utf8', function(err, json) {
 
   interactions = data.interactions;
 
-});
+}); 
+
 
 function get(method, path, body) {
-
+ 
+ 
   for (var i in interactions) {
 
     var interaction = interactions[i];
 
-    if (interaction.request.path.toString() === path) {
+
+    if (interaction.request.path.toString() === path && interaction.request.method.toString() === method) {
       var _body = interaction.request.body;
 
 
       var requestBody;
       if (body) {
         requestBody = JSON.parse(body);
-      }
+      } 
+      
 
-      if ((body && _body && JSON.stringify(_body) == JSON.stringify(requestBody)) || (!requestBody && !_body)) {
-
+      if ((body && _body && JSON.stringify(_body) == JSON.stringify(requestBody)) || (!requestBody && !_body) || (!body && !_body)) {
+        console.log("find response");
         return interaction.response;
       }
     }
@@ -73,11 +80,18 @@ http.createServer(function(req, res) {
   } else if (req.method == 'GET') {
     console.log("[200] " + req.method + " to " + req.url);
     
-    var response = get(req.method, req.url, body)
-    
-    res.writeHead(response.status, "OK", response.headers);
-    res.end(JSON.stringify(response.body));
+    var response = get(req.method, req.url)
 
+    if(response) {      
+
+      res.writeHead(response.status, "OK", response.headers);
+      res.end(JSON.stringify(response.body));
+    }else {
+        res.writeHead(404, "Bad Request", {
+          'Content-Type': 'application/json'
+        });
+        res.end('{"status":"loss test cases"}');
+      }
   }
   else {
     console.log("[405] " + req.method + " to " + req.url);
